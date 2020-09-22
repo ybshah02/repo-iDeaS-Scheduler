88a0ddc0c78fcfeaa2b361a08c274b764764b051
ybshah02 <65425667+ybshah02@users.noreply.github.com>
2020-09-22 14:51:11 -0500

    Add files via upload

---
+++ sql script/schedulerdb_client_lg_progression.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: schedulerdb
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client_lg_progression`
+--
+
+DROP TABLE IF EXISTS `client_lg_progression`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `client_lg_progression` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `client_id` int NOT NULL,
+  `learning_group_id` int NOT NULL,
+  `learning_session_id` int NOT NULL,
+  `finished` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`),
+  KEY `learning_group_id` (`learning_group_id`),
+  KEY `learning_session_id` (`learning_session_id`),
+  CONSTRAINT `client_lg_progression_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client_hotels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `client_lg_progression_ibfk_2` FOREIGN KEY (`learning_group_id`) REFERENCES `learning_groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `client_lg_progression_ibfk_3` FOREIGN KEY (`learning_session_id`) REFERENCES `learning_sessions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client_lg_progression`
+--
+
+LOCK TABLES `client_lg_progression` WRITE;
+/*!40000 ALTER TABLE `client_lg_progression` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client_lg_progression` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-22 14:48:35
